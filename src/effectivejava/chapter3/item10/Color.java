dff09531dfd12860cf3af76e19949185dc43193a
杉本　拓也 <33119022@stn.nitech.ac.jp>
2023-06-14 16:51:55 +0900

    change letter

--- src/effectivejava/chapter3/item10/Color.java
+++ src/effectivejava/chapter3/item10/Color.java
@@ -1,3 +1,3 @@
 package effectivejava.chapter3.item10;
 
-public enum Color { RED, ORANGE, YELLOW, GREEN, BLUE, INDIGO, VIOLET }
+public enum Color { red, orange, yellow, green, blue, indigo, violet }
